8be3815354eced2b7e43c5ef92674372d409d1ba
Shahzaibalikhawaja <shahzaib.ali.khawaja@gmail.com>
2024-05-09 00:43:45 +0500

    Util Code Updated

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler)=>{
+    (req, res,next ) => {
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err)=>next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+// higher order functions can accept functions as parameters and even return them
+
+
+
+// do method hain aik promise se aur dusra try catch se
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: true,
+//             message: err.message
+//         })
+//     }
+// }
